3de0ed3749621067cba181bb42fea8ba68ac8964
VICTOR <gurongai514@onet.pl>
2024-12-02 20:31:06 +0100

    Zdefiniowanie modelu danych treningu np: dystans, kalorie

---
+++ app/src/main/java/com/example/fitnesstracker/Training.kt
@@ -0,0 +1,9 @@
+package com.example.fitnesstracker
+
+data class Training
+    (val type: String,
+     val distance: Double,
+     val duration: Double,
+     val calories: Double,
+     val intensity: Int)
+
